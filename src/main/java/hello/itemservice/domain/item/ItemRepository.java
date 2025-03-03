7374571b52f6616ea82ff404b534085a234ef444
oheeseok <heeseok96@gmail.com>
2025-03-03 23:25:18 +0900

    상품 도메인 개발

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,47 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    // 동시에 여러 스레드가 접근하는 경우에는 HashMap 쓰면 안됨.
+    // ConcurrentHashMap 을 써야 함.
+    private static final Map<Long, Item> store = new HashMap<>(); // static
+    private static long sequence = 0L; // static
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+
+
+
+
+
+}
